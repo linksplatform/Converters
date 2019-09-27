c1ce2d761b11d3e6a4b131add078173153cab0ee
Konstantin Diachenko <drakonard@gmail.com>
2019-09-27 18:36:07 +0300

    IConverter interfaces moved from https://github.com/linksplatform/Interfaces

---
+++ IConverter[T].cs
@@ -0,0 +1,11 @@
+﻿namespace Platform.Converters
+{
+    /// <summary>
+    /// <para>Defines a converter between two values of the same type.</para>
+    /// <para>Определяет конвертер между двумя значениями одного типа.</para>
+    /// </summary>
+    /// <typeparam name="T"><para>Type of value to convert.</para><para>Тип преобразуемого значения.</para></typeparam>
+    public interface IConverter<T> : IConverter<T, T>
+    {
+    }
+}
