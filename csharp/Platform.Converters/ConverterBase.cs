c6c3269b001b0d3f8e69e17a589b55f220f462bb
Irin Alex Woods <77053698+dude-pocky@users.noreply.github.com>
2021-01-08 14:00:59 +0300

    Update ConverterBase.cs

--- csharp/Platform.Converters/ConverterBase.cs
+++ csharp/Platform.Converters/ConverterBase.cs
@@ -7,12 +7,24 @@ using Platform.Reflection;
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 namespace Platform.Converters
-{
+{   
+    /// <summary>
+    /// <para>Defines a converter between two types (TSource and TTarget).</para>
+    /// <para>Определяет конвертер между двумя типами (исходным TSource и целевым TTarget).</para>
+    /// </summary>
+    /// <typeparam name="TSource"><para>Source type of conversion.</para><para>Исходный тип конверсии.</para></typeparam>
+    /// <typeparam name="TTarget"><para>Target type of conversion.</para><para>Целевой тип конверсии.</para></typeparam>
     public abstract class ConverterBase<TSource, TTarget> : IConverter<TSource, TTarget>
     {
+        /// <summary>
+        /// <para>Converts the value of the source type (TSource) to the value of the target type.</para>
+        /// <para>Конвертирует значение исходного типа (TSource) в значение целевого типа.</para>
+        /// </summary>
+        /// <param name="source"><para>The source type value (TSource).</para><para>Значение исходного типа (TSource).</para></param>
+        /// <returns><para>The value is converted to the target type (TTarget).</para><para>Значение ковертированное в целевой тип (TTarget).</para></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public abstract TTarget Convert(TSource source);
-
+        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected static void ConvertFromObject(ILGenerator il)
         {
