548779bd92bb212385a2b7170f11c1ceb7aa2b9e
Irin Alex Woods <77053698+dude-pocky@users.noreply.github.com>
2021-01-10 15:26:50 +0300

    Update IConverter[TSource, TTarget].cs

--- csharp/Platform.Converters/IConverter[TSource, TTarget].cs
+++ csharp/Platform.Converters/IConverter[TSource, TTarget].cs
@@ -1,19 +1,19 @@
 ﻿namespace Platform.Converters
 {
     /// <summary>
-    /// <para>Defines a converter between two types (TSource and TTarget).</para>
-    /// <para>Определяет конвертер между двумя типами (исходным TSource и целевым TTarget).</para>
+    /// <para>Defines a value converter from the <typeparamref name="TSource"/> type to the <typeparamref name="TTarget"/> type.</para>
+    /// <para>Определяет конвертер значений из типа <typeparamref name="TSource"/> в тип <typeparamref name="TTarget"/>.</para>
     /// </summary>
     /// <typeparam name="TSource"><para>Source type of conversion.</para><para>Исходный тип конверсии.</para></typeparam>
     /// <typeparam name="TTarget"><para>Target type of conversion.</para><para>Целевой тип конверсии.</para></typeparam>
     public interface IConverter<in TSource, out TTarget>
     {
         /// <summary>
-        /// <para>Converts the value of the source type (TSource) to the value of the target type.</para>
-        /// <para>Конвертирует значение исходного типа (TSource) в значение целевого типа.</para>
+        /// <para>Converts the value of the <typeparamref name="TSource"/> type to the value of the <typeparamref name="TTarget"/> type.</para>
+        /// <para>Конвертирует значение типа <typeparamref name="TSource"/> в значение типа <typeparamref name="TTarget"/>.</para>
         /// </summary>
-        /// <param name="source"><para>The source type value (TSource).</para><para>Значение исходного типа (TSource).</para></param>
-        /// <returns><para>The value is converted to the target type (TTarget).</para><para>Значение ковертированное в целевой тип (TTarget).</para></returns>
+        /// <param name="source"><para>The <typeparamref name=="TSource"/> type value.</para><para>Значение типа <typeparamref name="TSource"/>.</para></param>
+        /// <returns><para>The converted value of the <typeparamref name="TTarget"/> type.</para><para>Значение конвертированное в тип <typeparamref name="TTarget"/>.</para></returns>
         TTarget Convert(TSource source);
     }
 }
