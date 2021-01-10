88f9ec8af55e1aa13cba59dfa3b0fbec57610bc0
Irin Alex Woods <77053698+dude-pocky@users.noreply.github.com>
2021-01-10 15:49:28 +0300

    Update IConverter[T].cs

--- csharp/Platform.Converters/IConverter[T].cs
+++ csharp/Platform.Converters/IConverter[T].cs
@@ -1,10 +1,10 @@
 ﻿namespace Platform.Converters
 {
     /// <summary>
-    /// <para>Defines a converter between two values of the same type.</para>
-    /// <para>Определяет конвертер между двумя значениями одного типа.</para>
+    /// <para>Defines a converter between two values of the same <typeparamref name="T"/> type.</para>
+    /// <para>Определяет конвертер между двумя значениями одного типа <typeparamref name="T"/>.</para>
     /// </summary>
-    /// <typeparam name="T"><para>Type of value to convert.</para><para>Тип преобразуемого значения.</para></typeparam>
+    /// <typeparam name="T"><para>The type of value to convert.</para><para>Тип преобразуемого значения.</para></typeparam>
     public interface IConverter<T> : IConverter<T, T>
     {
     }
