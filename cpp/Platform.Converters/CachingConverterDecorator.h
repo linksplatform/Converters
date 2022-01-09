0e2010c0f6faa6b02b507df381b38e81ba412a1b
Vladimir <58123600+Flakeed@users.noreply.github.com>
2022-01-09 18:00:06 +0200

    Update CachingConverterDecorator.h

--- cpp/Platform.Converters/CachingConverterDecorator.h
+++ cpp/Platform.Converters/CachingConverterDecorator.h
@@ -9,21 +9,21 @@ namespace Platform::Converters
     template <typename TSource, typename TTarget> class Cached<TSource, TTarget>
     {
     private: std::unique_ptr<std::function<TTarget(TSource)>> _baseConverter;
-    
+
     private: std::unordered_map<TSource, TTarget> _cache;
-    
+
     public: Cached(std::function<TTarget(TSource)>& baseConverter, std::unordered_map<TSource, TTarget>& cache) : _baseConverter(baseConverter), _cache(cache) {};
-    
+
     public: Cached(std::function<TTarget(TSource)>& baseConverter) : _baseConverter(baseConverter), _cache() {};
-    
-    public: TTarget operator()(const TSource& source){
+
+    public: TTarget operator()(const TSource& source) {
         if (auto cursor = _cache.find(source); cursor != _cache.end())
         {
             return *cursor;
         }
         else
         {
-            return *_cache.insert({ source, _baseConverter(source) })
+            return *_cache.insert({ source, _baseConverter(source) });
         }
     }
     };
