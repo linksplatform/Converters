c040357e032c8da1fcfc543a2b29962d9a497e6b
Vladimir <58123600+Flakeed@users.noreply.github.com>
2022-01-12 22:32:31 +0200

    Update CachingConverterDecorator.h

--- cpp/Platform.Converters/CachingConverterDecorator.h
+++ cpp/Platform.Converters/CachingConverterDecorator.h
@@ -1,25 +1,36 @@
 #pragma once
+
 #include <functional>
 #include <memory>
 #include <unordered_map>
 
 namespace Platform::Converters
 {
-    template <typename ...> class Cached;
-    template <typename TSource, typename TTarget> class Cached<TSource, TTarget>
+    template <typename TSource, typename TTarget, typename TCache = std::unordered_map<TSource, TTarget>> class Cached
     {
-    private: std::unique_ptr<std::function<TTarget(TSource)>> _baseConverter;
+    private: std::function<TTarget(TSource) > _baseConverter;
 
     private: std::unordered_map<TSource, TTarget> _cache;
 
     public: Cached(std::function<TTarget(TSource)>& baseConverter, std::unordered_map<TSource, TTarget>& cache) : _baseConverter(baseConverter), _cache(cache) {};
 
-    public: Cached(std::function<TTarget(TSource)>& baseConverter) : _baseConverter(baseConverter), _cache() {};
-
-    public: TTarget operator()(const TSource& source) {
+    public: TTarget operator()(TSource&& source)
+    {
         if (auto cursor = _cache.find(source); cursor != _cache.end())
         {
-            return *cursor;
+            return cursor->second;
+        }
+        else
+        {
+            return *_cache.insert({ source, _baseConverter(source) });
+        }
+    }
+
+    public: TTarget operator()(const TSource& source)
+    {
+        if (auto cursor = _cache.find(source); cursor != _cache.end())
+        {
+            return cursor->second;
         }
         else
         {
